f0fbe2c3081b9513ed12fd2fe1390d1113a00b07
neil dingeldein <ndingeldein@gmail.com>
2024-02-18 17:20:16 -0600

    clerk

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,11 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
+  printWidth: 100,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  semi: false,
+  tabWidth: 2,
+  trailingComma: "all",
   plugins: ["prettier-plugin-tailwindcss"],
 };
 
